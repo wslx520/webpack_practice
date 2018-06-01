da8954a0181470e425a0fc5b04e3b4cc6ae1d0ed
XLee <lix9nw@gmail.com>
2018-06-01 23:21:59 +0800

    this.resourcePath;.njk可编译了

--- config/loaders/nunjucks.loader.js
+++ config/loaders/nunjucks.loader.js
@@ -1,16 +1,26 @@
 const nunjucks = require('nunjucks');
 const loaderUtils = require("loader-utils");
+const  path = require('path')
 
 	// nunjucks.configure(options.views);
 	nunjucks.configure('./src');
+let hasSet = false;
+let viewRoot = null;
 const main = function (file) {
 	const options = loaderUtils.getOptions(this);
 
+	if (!hasSet) {
+		nunjucks.configure(options.views, options);
+		viewRoot = options.views;
+		hasSet = true;
+	}
 	this.cacheable();
-	console.log(Object.keys(this));
-	console.log('ooooooooooo',options,options.views);
-	console.log(file.path);
-	console.log(nunjucks.render(file, {name:'test'}))
+	const currentPath = this.resourcePath;
+	let all = Object.keys(this);
+	const relativePath = path.relative(viewRoot, currentPath);
+	console.log(currentPath, relativePath);
+	console.log('ooooooooooo',options);
+	console.log(nunjucks.render(relativePath, {name:'test'}))
 	return 'test'
 }
 
