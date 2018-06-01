4f28fda29fe1d76a5b32b15aedbc7b8703878e0d
wslx520 <lix9nw@gmail.com>
2018-06-01 19:37:35 +0800

    initial

--- config/loaders/nunjucks.loader.js
+++ config/loaders/nunjucks.loader.js
@@ -1,11 +1,15 @@
 const nunjucks = require('nunjucks');
 const loaderUtils = require("loader-utils");
 
-const main = function () {
+	nunjucks.configure(options.views);
+const main = function (file) {
 	const options = loaderUtils.getOptions(this);
 
 	this.cacheable();
-	console.log(options.path);
+	console.log('ooooooooooo',options,options.views);
+	console.log(file.path);
+	console.log(nunjucks.render(file, {name:'test'}))
+	return 'test'
 }
 
 module.exports = main;
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,32 @@
 const glob = require('fast-glob');
+const path = require('path');
 module.exports = {
-    entry() {
-        return new Promise(function (resolve, reject) {
-            return glob('./src/**/*.js');
-        })
+    entry: {
+        'index': './src/index.js'
+    },
+    // entry() {
+    //     return new Promise(function (resolve, reject) {
+    //         return glob('./src/**/*.js', (res) => {
+    //             console.log(res);
+    //             resolve(res);
+    //         });
+    //     })
+    // },
+    output: {
+        path: path.resolve('./dist'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [{
+            test: /\.njk$/,
+            use: [{
+                loader: path.resolve('./config/loaders/nunjucks.loader.js'),
+                options: {
+                    views: './src'
+                }    
+            }]
+            
+        }]
     },
     mode: 'development'
 }
