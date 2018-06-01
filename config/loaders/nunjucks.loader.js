aa4c2913358d74854f5023d56c78529e21a94187
wslx520 <lix9nw@gmail.com>
2018-06-01 19:49:02 +0800

    temp

--- config/loaders/nunjucks.loader.js
+++ config/loaders/nunjucks.loader.js
@@ -1,11 +1,13 @@
 const nunjucks = require('nunjucks');
 const loaderUtils = require("loader-utils");
 
-	nunjucks.configure(options.views);
+	// nunjucks.configure(options.views);
+	nunjucks.configure('./src');
 const main = function (file) {
 	const options = loaderUtils.getOptions(this);
 
 	this.cacheable();
+	console.log(Object.keys(this));
 	console.log('ooooooooooo',options,options.views);
 	console.log(file.path);
 	console.log(nunjucks.render(file, {name:'test'}))
